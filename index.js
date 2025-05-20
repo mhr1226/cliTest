b4dae95a878f260fabcfa42849458fa1889a391e
mhr1226 <h.m.michiharu@gmail.com>
2025-05-20 15:38:32 +0900

    add: 並列処理をpromise.allSettledに変更。 それに併せたエラー情報の保存情報の実装（htmlのみ）

--- index.js
+++ index.js
@@ -5,26 +5,47 @@ const CssCreator = require("./functions/CssCreator.js");
 const argv = require("./option.js");
 
 const createTest = async () => {
-
+  // 成功時の結果保存用の変数
   let results = {};
 
   try {
-
     // HTMLとCSSファイルを同時生成
-    const [htmlResult, cssResult] = await Promise.all([
+    const [htmlResult, cssResult] = await Promise.allSettled([
       HtmlCreator.createHtml(),
       CssCreator.createCss(),
     ]);
 
-    const htmlTotalResult = htmlResult.totalResult;
-    const cssTotalResult = cssResult.totalResult;
+    // エラー情報保存用の変数
+    const errors = [];
+
+    // 処理結果確認用の変数
+    let htmlSuccess = false;
+    let cssSuccess = false;
+
+    // HtmlCreatorの結果が失敗した場合
+    if (htmlResult.status === "rejected") {
+
+      // エラー情報を保存
+      const htmlError = {
+        name: "HTMLファイルの生成",
+        message: "HTMLファイルの生成に失敗しました。",
+        error: htmlResult.reason,
+      };
+      // エラー情報をエラーリストに追加
+      errors.push(htmlError);
+      console.log(errors);
+      console.error("HTMLファイルの生成に失敗しました。");
+    } else {
+      htmlSuccess = true;
+      console.log("HTMLファイルの生成に成功しました。");
+    }
 
     // 結果の保存
     results = {
       htmlResult,
       cssResult,
-    }
-    
+    };
+
     // 作成したCSSファイルをHTMLファイルに読み込む
     const loadCss = await HtmlCreator.loadCssoHtml({
       htmlFileName: htmlTotalResult.addExtAndPathResult.addExtResult.fileName,
@@ -35,11 +56,10 @@ const createTest = async () => {
     // 結果の保存
     results = {
       ...results,
-      loadCss
-    }
+      loadCss,
+    };
 
     console.log("全ての処理が完了したので、終了します。");
-
   } catch (err) {
     // 関数の終点
   }
